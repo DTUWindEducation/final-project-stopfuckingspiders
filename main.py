a74ebb767164d4148b741ed9ab6a040b930a6190
Dorian <s232808@dtu.dk>
2025-04-03 22:56:19 +0200

    add main.py with initial structure and import statements for data reading and plotting

---
+++ main.py
@@ -0,0 +1,30 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+import config as c
+
+#For Functions
+import re
+from typing import Union
+from pathlib import Path
+
+import WindTurbineModeling.read_data as WTMread
+
+
+#%% Functions
+def main():
+    pass
+
+#%% Read Data
+# Blade geometry (chord, twist, airfoil data along the span)
+
+# Operational conditions (wind speed, rotor speed, pitch angle)
+
+
+#%% Test for main
+if __name__ == "__main__":
+    pass
+
